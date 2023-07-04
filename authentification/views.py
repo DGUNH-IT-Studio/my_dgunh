b3f9ed5473c5b2b210b6945b2d4362a4405c5f3e
zhigibig <gamzat0004@mail.ru>
2023-07-04 13:16:59 +0300

    update .gitignore. create signup and login pages

--- authentification/views.py
+++ authentification/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
+    data = {
+        }
     return render(request, 'auth/index.html')
+
+def signup(request):
+    data = {
+        }
+    return render(request, 'auth/signup.html')
+
+def login():
+    data = {
+        }
+    return render(request, 'auth/login.html')
